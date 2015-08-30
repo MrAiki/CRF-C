66c197e16c11d2073f01261b9fbd90fd4ebb5137
Masatomo Aiki <vim_penjp_doujin_lover@ezweb.ne.jp>
2015-08-30 17:53:10 +0900

    first commit

---
+++ MEFeature.cpp
@@ -0,0 +1,71 @@
+#include "MEFeature.hpp"
+
+/* コンストラクタ. 重みはデフォルトで1にする. */
+MEFeature::MEFeature(int N_gram, int *pattern_x, int pattern_y, double weight=1.0)
+{
+  /* Nグラムのサイズのセット */
+  this->N_gram    = N_gram;
+
+  /* ユニグラム（前の単語に注目しない）以上ならば, pattern_xをセットする. */
+  if (N_gram > 1) {
+    this->pattern_x = new int[N_gram-1];
+    memcpy(this->pattern_x, pattern_x, sizeof(int) * (N_gram-1));
+  }
+  
+  /* 今の単語, 重みのセット */
+  this->pattern_y = pattern_y;
+  this->weight    = weight;
+}
+  
+/* デストラクタ */
+MEFeature::~MEFeature(void) 
+{
+  delete pattern_x;
+}
+  
+/* パターンチェックのサブルーチン. 
+   活性化していればtrue, していなければfalseを返す */
+bool MEFeature::check_pattern(int xlength, int *test_x, int test_y)
+{
+  /* 想定するxのパターン長さがこの素性以上, あるいはyのパターンが一致しなけばfalse */
+  if (xlength > N_gram || test_y != pattern_y) {
+    return false;
+  }
+
+  /* この素性のパターン長さが1（ユニグラム）ならば一致が確認できたのでtrue */
+  if (N_gram == 1) {
+    return true;
+  }
+
+  /* xのパターンを走査するしてチェック. */
+  for (int i=0; i < (N_gram-1); i++) {
+    if (test_x[i] != pattern_x[i]) {
+      return false;
+    }
+  }
+
+  /* 全ての一致が確認できたのでtrue */
+  return true;
+}
+
+/* パターンに対し活性化しているか調べ, 
+   活性化していればweightを返し, していなければ0を返す */
+double MEFeature::checkget_weight(int xlength, int *test_x, int test_y)
+{
+  if (check_pattern(xlength, test_x, test_y)) {
+    return weight; 
+  } else {
+    return 0.0f;
+  }
+}
+
+/* パターンに対し活性化しているか調べ, 
+   活性化していればparameter*weightを返し, していなければ0を返す */
+double MEFeature::checkget_param_weight(int xlength, int *test_x, int test_y)
+{
+  if (check_pattern(xlength, test_x, test_y)) {
+    return (parameter * weight);
+  } else {
+    return 0.0f;
+  }
+}
---
+++ MEFeature.hpp
@@ -0,0 +1,44 @@
+#ifndef MEFEATURE_H_INCLUDED
+#define MEFEATURE_H_INCLUDED
+
+#include <math.h>
+#include <string.h>
+
+/* Maximum Entropy Model （最大エントロピーモデル）の1つの素性を表現するクラス */
+class MEFeature {
+private:
+  int N_gram;            /* 素性のグラム数 */
+  int *pattern_x;        /* Nグラムの1つ前までの文字列. N_gram-1の長さの配列（単語は整数と対応付けている）
+		            pattern_x[0]=x_1, patern_x[1]=x_2, ... , pattern_x[N_gram-2]=x_{N_gram-1} */
+  int pattern_y;         /* Nグラムの今の単語 */
+
+public:
+  double weight;         /* 素性の重み */
+  int    count;          /* 素性のパターンが学習データで表れた回数 */
+  double empirical_prob; /* 素性の学習データにおける経験確率 */
+  double empirical_E;    /* この素性の経験期待値 */
+  double model_E;        /* モデルの期待値 */
+  double parameter;      /* 素性に付随する, モデルのパラメタ */
+  bool   is_marginal;    /* この素性が周辺素性（yのみに依存して活性化する素性）か否（条件付き素性; xにも依存して決まる素性）か */
+
+  /* コンストラクタ. */
+  MEFeature(int N_gram, int *pattern_x, int pattern_y, double weight);
+  /* デストラクタ. pattern_xの解放 */
+  ~MEFeature(void);
+
+  /* 以下, メソッド */
+public:
+  /* 仮引数のパターンtest_x,test_yに対してこの素性が活性化しているか調べ,
+     活性化していたらweightを返し, 活性化していなければ0を返す */
+  double checkget_weight(int xlength, int *test_x, int test_y);
+  /* 仮引数のパターンに対して活性化しているか調べ,
+     活性化していればweight*parameterを返す. （エネルギー関数; exp内部計算用）*/
+  double checkget_param_weight(int xlength, int *test_x, int test_y);
+  
+private:
+  /* パターンチェックのサブルーチン. 活性化していればtrue, していなければfalse */
+  bool check_pattern(int xlength, int *test_x, int test_y);
+
+};
+
+#endif /* MEFEATURE_H_INCLUDED */
